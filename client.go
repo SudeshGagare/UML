014a8b65d42c086e29aaf8b746f6f330ad63e93a
SudeshGagare <SudeshGagare@users.noreply.github.com>
2019-12-07 23:55:35 -0800

    Create client.go

---
+++ client.go
@@ -0,0 +1,161 @@
+interface ClientState {
+  frozen: boolean
+  pastPublicKeys: Set<PublicKey>
+  verifiedRoots: Map<uint64, CommitmentRoot>
+}
+
+interface ConsensusState {
+  sequence: uint64
+  publicKey: PublicKey
+}
+
+interface Header {
+  sequence: uint64
+  commitmentRoot: CommitmentRoot
+  signature: Signature
+  newPublicKey: Maybe<PublicKey>
+}
+
+interface Evidence {
+  h1: Header
+  h2: Header
+}
+
+// algorithm run by operator to commit a new block
+function commit(
+  commitmentRoot: CommitmentRoot,
+  sequence: uint64,
+  newPublicKey: Maybe<PublicKey>): Header {
+    signature = privateKey.sign(commitmentRoot, sequence, newPublicKey)
+    header = {sequence, commitmentRoot, signature, newPublicKey}
+    return header
+}
+
+// initialisation function defined by the client type
+function initialize(consensusState: ConsensusState): ClientState {
+  return {
+    frozen: false,
+    pastPublicKeys: Set.singleton(consensusState.publicKey),
+    verifiedRoots: Map.empty()
+  }
+}
+
+// validity predicate function defined by the client type
+function checkValidityAndUpdateState(
+  clientState: ClientState,
+  header: Header) {
+    abortTransactionUnless(consensusState.sequence + 1 === header.sequence)
+    abortTransactionUnless(consensusState.publicKey.verify(header.signature))
+    if (header.newPublicKey !== null) {
+      consensusState.publicKey = header.newPublicKey
+      clientState.pastPublicKeys.add(header.newPublicKey)
+    }
+    consensusState.sequence = header.sequence
+    clientState.verifiedRoots[sequence] = header.commitmentRoot
+}
+
+function verifyClientConsensusState(
+  clientState: ClientState,
+  height: uint64,
+  prefix: CommitmentPrefix,
+  proof: CommitmentProof,
+  clientIdentifier: Identifier,
+  consensusState: ConsensusState) {
+    path = applyPrefix(prefix, "clients/{clientIdentifier}/consensusState")
+    abortTransactionUnless(!clientState.frozen)
+    return clientState.verifiedRoots[sequence].verifyMembership(path, consensusState, proof)
+}
+
+function verifyConnectionState(
+  clientState: ClientState,
+  height: uint64,
+  prefix: CommitmentPrefix,
+  proof: CommitmentProof,
+  connectionIdentifier: Identifier,
+  connectionEnd: ConnectionEnd) {
+    path = applyPrefix(prefix, "connection/{connectionIdentifier}")
+    abortTransactionUnless(!clientState.frozen)
+    return clientState.verifiedRoots[sequence].verifyMembership(path, connectionEnd, proof)
+}
+
+function verifyChannelState(
+  clientState: ClientState,
+  height: uint64,
+  prefix: CommitmentPrefix,
+  proof: CommitmentProof,
+  portIdentifier: Identifier,
+  channelIdentifier: Identifier,
+  channelEnd: ChannelEnd) {
+    path = applyPrefix(prefix, "ports/{portIdentifier}/channels/{channelIdentifier}")
+    abortTransactionUnless(!clientState.frozen)
+    return clientState.verifiedRoots[sequence].verifyMembership(path, channelEnd, proof)
+}
+
+function verifyPacketCommitment(
+  clientState: ClientState,
+  height: uint64,
+  prefix: CommitmentPrefix,
+  proof: CommitmentProof,
+  portIdentifier: Identifier,
+  channelIdentifier: Identifier,
+  sequence: uint64,
+  commitment: bytes) {
+    path = applyPrefix(prefix, "ports/{portIdentifier}/channels/{channelIdentifier}/packets/{sequence}")
+    abortTransactionUnless(!clientState.frozen)
+    return clientState.verifiedRoots[sequence].verifyMembership(path, commitment, proof)
+}
+
+function verifyPacketAcknowledgement(
+  clientState: ClientState,
+  height: uint64,
+  prefix: CommitmentPrefix,
+  proof: CommitmentProof,
+  portIdentifier: Identifier,
+  channelIdentifier: Identifier,
+  sequence: uint64,
+  acknowledgement: bytes) {
+    path = applyPrefix(prefix, "ports/{portIdentifier}/channels/{channelIdentifier}/acknowledgements/{sequence}")
+    abortTransactionUnless(!clientState.frozen)
+    return clientState.verifiedRoots[sequence].verifyMembership(path, acknowledgement, proof)
+}
+
+function verifyPacketAcknowledgementAbsence(
+  clientState: ClientState,
+  height: uint64,
+  prefix: CommitmentPrefix,
+  proof: CommitmentProof,
+  portIdentifier: Identifier,
+  channelIdentifier: Identifier,
+  sequence: uint64) {
+    path = applyPrefix(prefix, "ports/{portIdentifier}/channels/{channelIdentifier}/acknowledgements/{sequence}")
+    abortTransactionUnless(!clientState.frozen)
+    return clientState.verifiedRoots[sequence].verifyNonMembership(path, proof)
+}
+
+function verifyNextSequenceRecv(
+  clientState: ClientState,
+  height: uint64,
+  prefix: CommitmentPrefix,
+  proof: CommitmentProof,
+  portIdentifier: Identifier,
+  channelIdentifier: Identifier,
+  nextSequenceRecv: uint64) {
+    path = applyPrefix(prefix, "ports/{portIdentifier}/channels/{channelIdentifier}/nextSequenceRecv")
+    abortTransactionUnless(!clientState.frozen)
+    return clientState.verifiedRoots[sequence].verifyMembership(path, nextSequenceRecv, proof)
+}
+
+// misbehaviour verification function defined by the client type
+// any duplicate signature by a past or current key freezes the client
+function checkMisbehaviourAndUpdateState(
+  clientState: ClientState,
+  evidence: Evidence) {
+    h1 = evidence.h1
+    h2 = evidence.h2
+    abortTransactionUnless(clientState.pastPublicKeys.contains(h1.publicKey))
+    abortTransactionUnless(h1.sequence === h2.sequence)
+    abortTransactionUnless(h1.commitmentRoot !== h2.commitmentRoot || h1.publicKey !== h2.publicKey)
+    abortTransactionUnless(h1.publicKey.verify(h1.signature))
+    abortTransactionUnless(h2.publicKey.verify(h2.signature))
+    clientState.frozen = true
+}
